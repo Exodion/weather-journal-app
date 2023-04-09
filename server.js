f2ef936c49771860949b439ed86120c86abfff54
Exodion <113134972+Exodion@users.noreply.github.com>
2023-04-09 10:17:39 +0200

    removed dataStore array

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
-dataStore = [];
+//dataStore = [];
 
 // Require Express to run server and routes
 const express = require("express");
